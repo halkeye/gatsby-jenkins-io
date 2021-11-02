9355a5041508a79508e90627c7949e1be58384d7
Gavin Mogan <git@gavinmogan.com>
2021-11-02 16:36:14 -0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
-import * as React from "react"
+import * as React from 'react';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import { StaticImage } from 'gatsby-plugin-image';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout notitle title="404: Not Found">
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <div className="errorcontainer" style={{ textAlign: 'center', margin: '30px' }}>
+      <StaticImage src="../../jenkins.io/content/images/fire-jenkins.svg.png" alt="jenkins on fire logo" />
+      <h1>The page you requested could not be found.</h1>
+      {/* TODO Add suggestions for frequently accessed pages, recently moved pages, etc.? */}
+    </div>
 
-export default NotFoundPage
+  </Layout>
+);
+NotFoundPage.displayName = 'NotFoundPage';
+
+export default NotFoundPage;
